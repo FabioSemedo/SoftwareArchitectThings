59e054b63fe2d77f80aae05814df73ca5076abde
FabioSemedo <fabio.ramos.semedo@gmail.com>
2026-06-29 23:03:29 +0100

    chore: complete EWMA analysis

--- agent_latency/README.md
+++ agent_latency/README.md
@@ -1,24 +1,51 @@
-This section aims to study how adequately the exponentialy weighted moving average (EWMA) estimates the latency of an agent's connection as well as in sample volumne impact the estimations.
+This section aims to study the adequacy of the Exponentially Weighted Moving Average ([EWMA](https://en.wikipedia.org/wiki/EWMA_chart)) in estimating the latency of an agent's connection and the impact of sample size on the accuracy of these estimations.
+   
+---
+
+Methodology, Data Collection and Visualisation
+
+- [x] Gather data from an idle agent
+- [x] Gather data from a busy agent (pubsub)Visualizations Developed
+- [x] Latency vs. Time
+- [x] EWMA vs. Time
+- [x] EWMA vs. Time with a Sample size 5 ($\text{EWMA}_{5}$)
+- [x] EWMA vs. Time with a Sample size 10 ($\text{EWMA}_{10}$)
+- [x] EWMA vs. Time with a Sample size 20 ($\text{EWMA}_{20}$)
+- [x] EWMA vs. Time with a Sample size 25 ($\text{EWMA}_{25}$)
+
+>Note: Because the server currently gathers the last 20 agent system metric datapoints, utilizing a sample size of $\leq$ 20 is optimal for this implementation.
 
 ---
 
-- [ ] Gather data from an idle agent
-- [ ] Gather data from a busy agent (pubsub)
+Conclusions
 
-Draw a gragh:
-  - [ ] Latency vs Time
-  - [ ] EWMA vs Time
-  - [ ] EWMA vs Time with a Sample size 5 $EWMA_{5}$
-  - [ ] EWMA vs Time with a Sample size 10 $EWMA_{10}$
-  - [ ] EWMA vs Time with a Sample size 20 $EWMA_{20}$
-  - [ ] EWMA vs Time with a Sample size 10 $EWMA_{25}$
->Note: since the server is already gathering the last 20 agent system metric datapoints, using a sample of size $\leq$ 20 would be ideal
+An analysis of the [graphs](./images/) reveals that the standard EWMA, with $\alpha = 0.125$ tends to underestimate agent latency by a considerable margin:
+- ~15% underestimation for a sample with a fixed trend.
+- ~30% underestimation for a sample with fluctuating trends.
 
-Evaluate accuracy:
-  - [ ] Compared to EWMA that uses the entire dataset, we will measure the margin of error of EWMA$_i$ that uses a subset of the dataset:
-  > margin = 1.00 -  abs($EWMA_i - EWMA$)
+To mitigate this discrepancy and decrease the margin we have a few options, 
+1. Increase $\alpha$
+ 
+The default $\alpha$ is 0.125. Testing shows $\alpha$ = 0.3 decreases the margin between true latency and the EWMA estimation to bellow 5%. As well as causing sample sizes to have less influce in the yield.  
+I.e. $\text{EWMA}_5 \approx \text{EWMA}_{20} \approx \text{EWMA}$
 
-Segment the dataset to study how the subsets handle:
-    - [ ] 1 trend in the latency (1 segment)
-    - [ ] A change in the trend  (between segments)
-  
+2. Decrease the sample size, $i$, (with $\alpha$ = 0.125) provided to the EWMA calculation. 
+
+The resulting delta between the true latency and the $\text{EWMA}_i \land \alpha = 0.125$ estimation will decrease as $i$ decreases:
+
+Sample size 5: Delta is < 5%                                    [View ewma_5](./images/temp_ewma_05.png)  
+Sample size 10: Delta is closely aligned with the standard EWMA [View ewma_10](./images/temp_ewma_10.png)  
+Sample size 20: Delta is approximately equal to the standard EWMA             [View ewma_20](./images/temp_ewma_20.png)  
+
+---
+
+Limitations & Future Considerations
+
+**Clock Offset Bias**: The current data contains a bias due to clock offsets. When the system clock corrects itself the measured latency drops dramatically. This causes the measured latency to range from roughly 0.1s to 1.0s every ~2s. These clock corrections artificially impact the benchmark because the server currently resides on the same machine as the agent.
+
+**Production Environment**: Once the Robotair infrastructure is deployed to a production environment, the agents will typically run on machines separate from the server. While this physical separation will inherently increase the baseline agent latency, it will also significantly decrease the influence of the clock fluctuations observed in our current measurements.  
+  This means that estimations like $\text{EWMA}_5$ and $\text{EWMA}_{10}$ would server as better indicators of the agent's connection's health.
+
+Data and Graphs can be found here:  
+[Data](./data/)  
+[Graphs](./images/)
--- agent_latency/main.py
+++ agent_latency/main.py
@@ -1,17 +1,22 @@
 import json
 import matplotlib.pyplot as plt
 import numpy as np
-
-from time import time
+import datetime
+from pprint import pprint
 from typing import Dict, List, Tuple
 
 from ewma import estimate_agent_latency
 from ewma import _calculate_ewma
 
+ALPHA = 0.3
+
+IDLE_FILE = "idle_agent_1.json"
+BUSY_FILE = "busy_agent_1.json"
+
 def printH(header):
-    print("___________________________________________________________")
+    print("________________________________________________________")
     print(header)
-    print("___________________________________________________________")
+    print("________________________________________________________")
 
 def test_estimate_agent_latency():
     printH("Testing estimate_agent_latency:")
@@ -33,33 +38,42 @@ def test_estimate_agent_latency():
     result_125 = estimate_agent_latency(data, newest_first=True, alpha=0.125)
     print("alpha = 0.125", data, result_125)
 
+# -----------------------------------------------------------------------------------------
+# Prepare data
+# -----------------------------------------------------------------------------------------
+
 # Compute EWMA_i where i = step = size of sample given to EWMA
 def calculate_ewma_i(buf: list, data: list, step: int):
-        j = 0
-        buf.append(estimate_agent_latency(data[:step], newest_first=False, max_sample_size=step + 1))
-        for i in range(step*2, len(data), step):
-            j += step
-            buf.append(estimate_agent_latency(data[j:i], newest_first=False, max_sample_size=step +1))
+    j = 0
+    buf.append(estimate_agent_latency(data[:step], newest_first=False, max_sample_size=step + 1, alpha=ALPHA))
+    for i in range(step*2, len(data)+1, step):
+        j += step
+        buf.append(estimate_agent_latency(data[j:i], newest_first=False, max_sample_size=step +1, alpha=ALPHA))
 
             
 def read_dataset(data_file: str, newest_is_frist: bool) -> Tuple[List[float], List[float]]:
+    """
+    Example
+    -------
+    >>> {"data":[["2026-06-26 13:38:45.743316+00","0.05012702941894531"],["2026-06-26 13:38:35.908915+00","0.22125792503356934"]]}
+    [1782481125.743316, 1782481115.908915], [50.12702941894531, 221.25792503356934]
+    """
     printH(f"Testing estimate_agent_latency with real data: {data_file}")
 
 
-    with open(f"data/{data_file}", "r") as file:
-        data: Dict[str, List[Tuple[float, float]]] = json.loads(file.read())
+    with open("./data/"+data_file, "r") as file:
+        data: Dict[str, List[Tuple[str, float]]] = json.loads(file.read())
 
-    print(data)
     if newest_is_frist:
         data["data"].reverse()
 
-    timestamp = [x[0] for x in data["data"]]
-    latency = [x[1]*1000 for x in data["data"]] # convert to ms
+    timestamp = [datetime.datetime.fromisoformat(x[0]).timestamp() for x in data["data"]]        
+    latency = [float(x[1])*1000 for x in data["data"]] # convert to ms
 
     return (latency, timestamp)
 
-def wrap_dataset(buffer:list, timestamps: list, step:int ):
-    print(f"EWMA_{step}: ", buffer, "\nlength: ", len(buffer))
+def zip_timestamp_with_ewma(buffer:List[float], timestamps: List[float], step:int) -> List[Tuple[float, float]]:
+    print(f"EWMA_{step}: length=", len(buffer))
 
     arr = []
     time_idx = step-1
@@ -71,56 +85,53 @@ def wrap_dataset(buffer:list, timestamps: list, step:int ):
 
     return arr
 
-data, timestamp = read_dataset("idle_1.json", newest_is_frist=True)
+# -----------------------------------------------------------------------------------------
+# Visuals
+# -----------------------------------------------------------------------------------------
 
-ewma = _calculate_ewma(data)
-ewma_ds = wrap_dataset(ewma, timestamp, 1)
+def add_to_graph(data:List[Tuple[float, float]], scatter: bool,  color:str, label:str):
+        x_data = np.array([x[0] for x in data])
+        y_data = np.array([x[1] for x in data])
 
-ewma_10: List[float] = []
-step = 10
+        if scatter:
+            plt.scatter(x_data, y_data, color=color, label=label)
+        else:
+            plt.plot(x_data, y_data, color=color, label=label)
 
-calculate_ewma_i(buf=ewma_10, data=data, step=step)
-
-
-def graph():
-    # Generate sample data for distinct groups
-    x_data_1 = np.array(timestamp)
-    y_data_1 = np.array(data)  
-
-    x_data_2 = np.array(timestamp)
-    y_data_2 = np.array(ewma)   
-
-    buf = ewma_10
-    x_data_3 = np.array([x for x in range(int(200/len(buf)),201, int(200/len(buf)))])
-    y_data_3 = np.array(buf)
-
-    plt.plot(x_data_1, y_data_1, color='blue', label='Agent heartbeep latency')
-    plt.plot(x_data_2, y_data_2, color='red', label='EWMA latency estimation')
-
-    # # Calculate the trend lines
-    # # np.polyfit(x, y, 1) calculates a 1st-degree polynomial (a linear trend line)
-    # fit_1 = np.polyfit(x_data_1, y_data_1, 1)
-    # line_1 = np.poly1d(fit_1)
-
-    # fit_2 = np.polyfit(x_data_2, y_data_2, 1)
-    # line_2 = np.poly1d(fit_2)
-
-    # # Plot the trend lines
-    # # We pass the x-data and the calculated y-values from our poly1d functions
-    # plt.plot(x_data_1, line_1(x_data_1), color='blue', linestyle='--', label='Group A Trend')
-    # plt.plot(x_data_2, line_2(x_data_2), color='red', linestyle='--', label='Group B Trend')
-
-    # Draw the scatter plots
-    # plt.scatter(x_data_1, y_data_1, color='blue', label='Heartbeep delta time')
-    # plt.scatter(x_data_2, y_data_2, color='red', label='EWMA')
-    plt.scatter(x_data_3, y_data_3, color='green', label=f'EWMA_{step} subset')
-
+def save_graph():
     # Format and label the chart
-    plt.title('Benchmarking EWMA_i estimation of the Robotiar agent latency')
-    plt.xlabel('Time (in s)')
+    plt.title(f'Benchmarking EWMA_i estimation of the Robotiar agent latency with Alpha = {ALPHA}')
+    plt.xlabel('Time (in ms)')
     plt.ylabel('Latency (in ms)')
     plt.legend()
     plt.grid(True, linestyle=':', alpha=0.6)
 
-    # 6. Render the plot
-    plt.savefig(fname=f"images/temp_{int(time())}.png")
+    fname = f"images/temp_{int(datetime.datetime.now().timestamp())}.png"
+    # fname = f"images/temp.png"
+    plt.savefig(fname=fname, dpi=300, bbox_inches='tight')
+
+if __name__=="__main__":
+    plt.figure(figsize=(12, 6))
+
+    data, timestamp = read_dataset(IDLE_FILE, newest_is_frist=True)
+    start = timestamp[0]
+    timestamp = [round(timestamp[i] - start, 4) for i in range(len(timestamp))]
+    add_to_graph(list(zip(timestamp, data)), scatter=False, color= "#1A1A1A", label="Latency")
+    
+    ewma = _calculate_ewma(data, alpha=ALPHA)
+    ewma_ds = zip_timestamp_with_ewma(ewma, timestamp, 1)
+    add_to_graph(ewma_ds, scatter=False, color="blue", label="EWMA")
+
+    def ewma_i(step: int, color:str):
+        ewma_i: List[float] = []
+        step = step
+        calculate_ewma_i(buf=ewma_i, data=data, step=step)
+        ewma_i_ds = zip_timestamp_with_ewma(ewma_i, timestamp, step)
+        add_to_graph(ewma_i_ds, scatter=True, color=color, label=f"EWMA_{step}")
+
+    ewma_i(5 , "#a60000")
+    ewma_i(10, "#E00000")
+    # ewma_i(15, "#459000")
+    ewma_i(20, "#02e132")
+    # ewma_i(25, "#6704a5")
+    save_graph()
